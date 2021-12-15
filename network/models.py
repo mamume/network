c4dbc19a207335cff0b52513b2193e21ab870332
mamume <mmustafametwally@gmail.com>
2021-12-15 14:35:40 +0200

    alter follow relation

--- network/models.py
+++ network/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    followers = models.ManyToManyField('User')
+    followers = models.ManyToManyField('self')
 
 
 class Post(models.Model):
