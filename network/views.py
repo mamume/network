5a101c25487932d6b58f8c4665483e8c495e8790
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-12-15 22:08:31 +0200

    add paginator to index view and posts.html

--- network/views.py
+++ network/views.py
@@ -1,17 +1,23 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
 
 from .models import Post, User
 
 
 def index(request):
     posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request, "network/index.html", context={
-        "posts": posts
+        # "posts": posts
+        'page_obj': page_obj
     })
 
 
