b358ebe0eaf8cd62e25d1a98eb1a6b7c3f421f78
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-12-15 07:40:58 +0200

    makemigrations, add DEAFULT_AUTO_FIELD, add python formatter lib

--- project4/settings.py
+++ project4/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEAFULT_AUTO_FIELD = django.db.models.BigAutoField
