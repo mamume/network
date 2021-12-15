aa1bf1b592c61f672486fc4f405d2315f4b228a7
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-12-15 09:16:50 +0200

    create comment model

--- network/models.py
+++ network/models.py
@@ -12,4 +12,12 @@ class Post(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name="liked_posts")
-    comments = models.ManyToManyField(User, related_name="commented_posts")
+    # comments = models.ManyToManyField(User, related_name="commented_posts")
+
+
+class Comment(models.Model):
+    owner = models.ManyToManyField(User)
+    post = models.ManyToManyField(Post)
+
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
