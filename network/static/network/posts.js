7409d2da0a8eccaaae30c1926bc3cb6c8d893085
Mahmoud Metwally <mmustafametwally@gmail.com>
2021-12-16 14:01:41 +0200

    add rest_framework app

---
+++ network/static/network/posts.js
@@ -0,0 +1,72 @@
+// Source => https://stackoverflow.com/questions/40893537/fetch-set-cookies-and-csrf
+function getCookie(name) {
+  if (!document.cookie) {
+    return null;
+  }
+
+  const xsrfCookies = document.cookie.split(';')
+    .map(c => c.trim())
+    .filter(c => c.startsWith(name + '='));
+
+  if (xsrfCookies.length === 0) {
+    return null;
+  }
+  return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+}
+
+function editPost(postId, parent) {
+  editedText = document.querySelector(`#edit-post-${postId}`).value.trim()
+  data = {
+    id: postId,
+    text: editedText
+  }
+
+  fetch('http://localhost:8000/edit/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json; charset=UTF-8',
+      'X-CSRFToken': getCookie('csrftoken')
+    },
+    body: JSON.stringify(data),
+  })
+    .then(res => res.json())
+    .then(data => {
+      parent.innerHTML = `
+        <a href={% url 'profile' post.owner.id %}><b>{{ post.owner }}</b></a>
+        {% if request.user == post.owner %}
+          <a href="#edit" class="edit-link">Edit</a>
+          <input class="post-id" type="hidden" value={{ post.id }} />
+        {% endif %}
+        <div class="post-text">{{ post.text }}</div>
+        <div class="text-secondary">
+            <div>{{ post.created_at }}</div>
+            <div>💖 {{ post.likes.count }}</div>
+            <div>Comment</div>
+        </div>
+      `
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  editLinks = document.querySelectorAll('.edit-link')
+
+  for (const editLink of editLinks) {
+    editLink.addEventListener('click', () => {
+      const parent = editLink.parentNode
+      const postText = parent.querySelector('.post-text').innerText
+      const postId = parent.querySelector(".post-id").value
+
+      parent.innerHTML = `
+          <div class="mb-3">
+            <label for="edit-post-${postId}" class="form-label">Edit Post</label>
+            <textarea id="edit-post-${postId}" name="edited-text" class="form-control" rows="3">
+              ${postText}
+            </textarea>
+            <input type="hidden" name="postId" value=${postId} />
+          </div>
+          <button onclick="editPost(${postId}, ${parent})" class="btn btn-primary">Save</button>
+      `
+    })
+  }
+})
